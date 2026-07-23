23d34ea22dbf815dc91aaecbe104debc8dd7aaaf
macias-german <macias.german@outlook.com>
2026-07-23 16:12:09 -0300

    add: header and write area component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.webp";
+import themeIcon from "../assets/theme-icon.webp";
+
+const Header = () => {
+    return (
+        <header>
+            <hgroup>
+                <img src={logo} alt="Logo de la aplicacion Character Counter." />
+                <h2>Character Counter</h2>
+            </hgroup>
+            <button><img src={themeIcon} alt="Imagen del cambio de tema claro/oscuro." /></button>
+        </header>
+    )
+}
+
---
+++ src/components/WriteArea.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+const WriteArea = () => {
+    const [text, setText] = useState("")
+
+    return (
+        <div class="write-area">
+            <h1>Analyze your text in real-time.</h1>
+            <textarea
+                placeholder="Type your text here..."
+                onChange={(e) => setText(e.target.value)}
+                value={text}>
+            </textarea>
+        </div>
+    )
+}
